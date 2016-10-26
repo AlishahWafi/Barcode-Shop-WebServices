9b9548468ed8a6b2df0b50cf3ab579902b2f3e5c
w <w@ALISHAH>
2016-10-26 23:04:02 +0500

    Add project files.

---
+++ Shopping/Controllers/loginController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using Shopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Shopping.Controllers
+{
+    public class loginController : ApiController
+    {
+        DBShoppingEntities db = new DBShoppingEntities();
+
+        [HttpPost]
+        public IHttpActionResult login(user u)
+        {
+            var detail = db.users.Where(x => x.Username == u.Username && x.Password == u.Password).FirstOrDefault();
+
+            if (detail != null)
+            {                
+                return Ok(JObject.Parse("{success : true}"));
+            }
+            else
+            {               
+                return Ok(JObject.Parse("{success : false}"));
+            }
+        }
+    }
+}
---
+++ Shopping/Controllers/registerController.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using Shopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Shopping.Controllers
+{
+    public class registerController : ApiController
+    {
+        DBShoppingEntities db = new DBShoppingEntities();
+
+        [HttpPost]
+        public IHttpActionResult register(user u)
+        {
+            var detail = db.users.Where(x => x.Username == u.Username).FirstOrDefault();
+            if (detail == null)
+            {
+                db.users.Add(u);
+                db.SaveChanges();
+
+                return Ok(JObject.Parse("{success : true}"));
+            }
+            else
+            {
+                return Ok(JObject.Parse("{success : false}"));
+            }
+        }
+    }
+}
